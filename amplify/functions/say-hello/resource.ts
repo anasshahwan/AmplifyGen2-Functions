d319e3fdf7adc3ce6a9290c990691a94a3beee29
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-06 15:35:35 -0400

    update lambda configurations

--- amplify/functions/say-hello/resource.ts
+++ amplify/functions/say-hello/resource.ts
@@ -3,4 +3,9 @@ import { defineFunction } from '@aws-amplify/backend';
 export const sayHello = defineFunction({
   name: 'sayHello',
   entry: './handler.ts',
+  timeoutSeconds: 60, //1 minute
+  memoryMB: 256, //128 MB
+  ephemeralStorageSizeMB: 512, //512 MB
+  runtime: 22,
+  resourceGroupName: 'data',
 });
